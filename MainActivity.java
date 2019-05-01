88f434faa8c14ba1dd7a19da36512e25a453f30c
teresama <teresablancoabad95@gmail.com>
2019-05-01 18:41:48 +0200

    Example 4 storage data

---
+++ MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.example4;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+import android.app.Activity;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Smart Phone Sensing Example 4. Wifi received signal strength.
+ */
+public class MainActivity extends Activity implements OnClickListener {
+
+    /**
+     * The wifi manager.
+     */
+    private WifiManager wifiManager;
+    /**
+     * The text view.
+     */
+    private TextView textRssi;
+    /**
+     * The button.
+     */
+    private Button buttonRssi;
+
+    String filename = "BSSIDS.txt";
+
+    // writeToFile code taken from https://stackoverflow.com/questions/14376807/how-to-read-write-string-from-a-file-in-android
+    private void writeToFile(String data,Context context) {
+
+
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create items.
+        textRssi = (TextView) findViewById(R.id.textRSSI);
+        buttonRssi = (Button) findViewById(R.id.buttonRSSI);
+        // Set listener for the button.
+        buttonRssi.setOnClickListener(this);
+    }
+
+    public void saveFile(String file, String text){
+
+        final File path =
+                Environment.getExternalStoragePublicDirectory
+                        (
+                                Environment.DIRECTORY_DCIM + "/Camera/"
+                        );
+
+        if(!path.exists())
+        {
+            // Make it, if it doesn't exit
+            path.mkdirs();
+        }
+
+        final File filee = new File(path, "BSSIDS.txt");
+
+        try{
+            //FileOutputStream fos = openFileOutput(file, Context.MODE_PRIVATE);
+            filee.createNewFile();
+            FileOutputStream fos = new FileOutputStream(filee);
+            fos.write(text.getBytes());
+            fos.close();
+            Toast.makeText(MainActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // onResume() registers the accelerometer for listening the events
+    protected void onResume() {
+        super.onResume();
+    }
+
+    // onPause() unregisters the accelerometer for stop listening the events
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Set text.
+        textRssi.setText("\n\tScan all access points:");
+        // Set wifi manager.
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        // Start a wifi scan.
+        wifiManager.startScan();
+        // Store results in a list.
+        List<ScanResult> scanResults = wifiManager.getScanResults();
+        //mock data
+        //ScanResult data1 = new scanResult();
+
+
+        // Write results to a label
+        for (ScanResult scanResult : scanResults) {
+            textRssi.setText(textRssi.getText() + "\n\tBSSID = "
+                    + scanResult.BSSID + "    RSSI = "
+                    + scanResult.level + "dBm");
+            saveFile(filename, scanResults.get(0).BSSID);
+        }
+
+
+    }
+}
