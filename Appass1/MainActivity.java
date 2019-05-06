59b2935e3ae5b2d08701941b49f4e8360e032c26
gerardomoyers <moyersbg@gmail.com>
2019-05-06 18:43:28 +0200

    Add Training data cafe, 20 scans per cell.

--- Appass1/MainActivity.java
+++ Appass1/MainActivity.java
@@ -73,10 +73,15 @@ public class MainActivity extends Activity implements OnClickListener, SensorEve
      */
     private final String[][] trainingArray = new String[][]//static to determine where I am
             {
-                    new String[] {"00:14:5c:8e:94:08","8c:68:c8:88:b7:ec","bc:1c:81:28:df:0a","8c:68:c8:88:b7:ed","70:3a:cb:f5:ec:1f","70:3a:cb:f5:ed:d3","70:3a:cb:f5:ec:1b","00:71:c2:0d:5a:29","70:3a:cb:f5:ed:d7","d4:6e:0e:36:e4:3d","30:b5:c2:22:d1:46","70:3a:cb:fa:c8:27","74:85:2a:18:5e:32","1c:74:0d:3b:98:16","00:0c:f6:cb:04:cc","00:0f:94:b3:cf:c4","d2:6e:0e:36:e4:3e","d4:6e:0e:36:e4:3e","70:3a:cb:fa:c8:23","64:d1:a3:26:90:7e","70:3a:cb:f5:eb:b1","88:03:55:ee:f9:4d","00:0f:94:b3:ca:74","80:2a:a8:d1:26:54","70:3a:cb:fa:c4:54","72:3a:cb:f9:15:15","bc:30:7e:38:ec:81","2a:a1:83:9b:2e:2b","72:3a:cb:f9:15:d9","74:a7:8e:e1:1d:f0","62:a7:8e:e1:1d:f1","00:0f:94:b3:cb:44","72:df::70:a3:68:5d","e8:df:70:a3:68:5c","dc:9f:db:64:2e:b8"},
+                   /* new String[] {"00:14:5c:8e:94:08","8c:68:c8:88:b7:ec","bc:1c:81:28:df:0a","8c:68:c8:88:b7:ed","70:3a:cb:f5:ec:1f","70:3a:cb:f5:ed:d3","70:3a:cb:f5:ec:1b","00:71:c2:0d:5a:29","70:3a:cb:f5:ed:d7","d4:6e:0e:36:e4:3d","30:b5:c2:22:d1:46","70:3a:cb:fa:c8:27","74:85:2a:18:5e:32","1c:74:0d:3b:98:16","00:0c:f6:cb:04:cc","00:0f:94:b3:cf:c4","d2:6e:0e:36:e4:3e","d4:6e:0e:36:e4:3e","70:3a:cb:fa:c8:23","64:d1:a3:26:90:7e","70:3a:cb:f5:eb:b1","88:03:55:ee:f9:4d","00:0f:94:b3:ca:74","80:2a:a8:d1:26:54","70:3a:cb:fa:c4:54","72:3a:cb:f9:15:15","bc:30:7e:38:ec:81","2a:a1:83:9b:2e:2b","72:3a:cb:f9:15:d9","74:a7:8e:e1:1d:f0","62:a7:8e:e1:1d:f1","00:0f:94:b3:cb:44","72:df::70:a3:68:5d","e8:df:70:a3:68:5c","dc:9f:db:64:2e:b8"},
                     new String[] {"00:14:5c:8e:94:08","8c:68:c8:88:b7:ec","bc:1c:81:28:df:0a","8c:68:c8:88:b7:ed","64:d1:a3:26:90:7e","00:71:c2:0d:5a:29","1c:74:0d:3b:98:16","00:0f:94:b3:cf:c4","30:b5:c2:22:d1:46","00:71:c2:0d:5c:c1","70:3a:cb:f5:ed:d7","80:2a:a8:d1:26:54","74:85:2a:18:5e:32","00:14:5c:97:8b:3e","62:a7:8e:e1:1d:f1","70:3a:cb:f5:ec:1f","00:18:4d:92:8d:c8","74:a7:8e:e1:1d:f0","00:0c:f6:cb:04:cc","70:3a:cb:f5:eb:b1","00:0f:94:b3:cb:44","88:03:55:ee:f9:4d","e8:df:70:a3:68:5c","d4:6e:0e:36:e4:3e","70:3a:cb:fa:c8:27","d2:6e:0e:36:e4:3e","72:df::70:a3:68:5d","fc:ec:da:8a:31:f6","00:0f:94:b3:ca:74","70:3a:cb:f5:ed:d3","70:3a:cb:f5:ec:1b","d4:6e:0e:36:e4:3d"},
                     new String[] {"00:14:5c:8e:94:08","bc:1c:81:28:df:0a","8c:68:c8:88:b7:ec","64:d1:a3:26:90:7e","8c:68:c8:88:b7:ed","70:3a:cb:fa:c8:27","00:71:c2:0d:5a:29","00:0f:94:b3:cf:c4","70:3a:cb:f5:ed:d7","1c:74:0d:3b:98:16","00:71:c2:0d:5c:c1","74:85:2a:18:5e:32","70:3a:cb:f5:ec:1f","30:b5:c2:22:d1:46"},
                     new String[] {"00:14:5c:8e:94:08","70:3a:cb:f5:ed:d7","8c:68:c8:88:b7:ec","64:d1:a3:26:90:7e","bc:1c:81:28:df:0a","e0:28:6d:a3:99:7b","70:3a:cb:fa:c8:27","94:6a:b0:d0:90:11","20:4e:7f:22:ef:dc","cc:ce:1e:e0:6e:a1","44:d9:e7:fd:f3:94","70:3a:cb:f5:ed:d3","70:3a:cb:fa:c4:54"}
+*/
+                    new String[] {"70:db:98:26:7c:5f","70:db:98:26:7c:5d","70:db:98:26:7c:5e","00:a3:8e:22:9c:6f","00:a3:8e:22:9c:6d","00:a3:8e:22:9c:6e","70:db:98:26:7c:50","70:db:98:26:7c:52","70:db:98:26:7c:51","2c:33:11:43:07:4f","2c:33:11:43:07:4d","2c:33:11:43:07:41","2c:33:11:43:1d:4f","2c:33:11:43:1d:4d","2c:33:11:43:1d:4e","2c:33:11:43:1d:41","2c:33:11:52:23:61","2c:33:11:52:23:62","08:96:ad:a0:6c:ce","2c:33:11:52:23:60","00:a3:8e:22:9c:61","2c:d0:2d:98:16:ce","2c:33:11:50:2c:9e","2c:33:11:50:2c:9f","2c:33:11:50:2c:9d","2c:d0:2d:98:16:cf","2c:d0:2d:98:16:cd","00:a3:8e:22:9c:60","00:a3:8e:22:9c:62","2c:33:11:50:2d:00","2c:33:11:50:2d:01","2c:33:11:50:2d:0e","2c:33:11:50:2d:0d","2c:d0:2d:f5:7a:2e","2c:d0:2d:f5:7a:2f","2c:d0:2d:f5:7a:2d","2c:33:11:50:2c:92","2c:33:11:43:07:4e","2c:33:11:50:2c:91","2c:33:11:50:2c:90","2c:d0:2d:98:16:c1","50:1c:bf:e2:32:2f","50:1c:bf:e2:32:2d","50:1c:bf:e2:32:2e","2c:33:11:56:52:bf"},
+                    new String[] {"00:a3:8e:22:9c:6f","00:a3:8e:22:9c:6e","00:a3:8e:22:9c:6d","2c:33:11:43:1d:4f","2c:33:11:43:1d:4d","2c:33:11:43:1d:4e","70:db:98:26:7c:51","2c:33:11:43:1d:41","70:db:98:26:7c:5f","70:db:98:26:7c:5d","70:db:98:26:7c:5e","2c:d0:2d:98:16:cd","00:a3:8e:8f:b4:4f","2c:33:11:52:23:6f","70:db:98:26:7c:50","70:db:98:26:7c:52","00:a3:8e:8f:b4:4d","00:a3:8e:8f:b4:4e","00:a3:8e:8f:be:7f","00:a3:8e:8f:be:7d","00:a3:8e:8f:be:7e","2c:33:11:22:eb:2f","2c:33:11:22:eb:2d","2c:33:11:22:eb:2e","2c:33:11:22:ea:ff","2c:33:11:22:ea:fd","2c:33:11:22:ea:fe","2c:33:11:43:07:4f","2c:33:11:43:0b:9e","2c:33:11:43:05:ee","54:4a:00:66:12:ad"},
+                    new String[] {"70:db:98:26:7c:5f","70:db:98:26:7c:5d","70:db:98:26:7c:5e","70:db:98:26:7c:52","70:db:98:26:7c:51","00:a3:8e:8f:be:7f","00:a3:8e:8f:be:7d","00:a3:8e:8f:be:7e","00:a3:8e:22:9c:6f","00:a3:8e:8f:b4:4d","00:a3:8e:8f:b4:4e","2c:33:11:43:1d:4f","2c:33:11:43:1d:4d","2c:33:11:43:1d:4e","00:a3:8e:22:9c:6d","00:a3:8e:22:9c:6e","2c:33:11:43:05:ee","2c:33:11:22:eb:2f","2c:33:11:22:eb:2e","2c:33:11:43:05:ed","2c:33:11:22:eb:2d","2c:33:11:43:05:ef","54:4a:00:66:12:ae","54:4a:00:66:12:ad","54:4a:00:66:12:af","00:a3:8e:8f:be:72","00:a3:8e:8f:be:71","00:a3:8e:8f:be:70","2c:33:11:43:05:e1","2c:33:11:43:05:e0","2c:33:11:43:05:e2","2c:33:11:43:09:7e","2c:33:11:43:09:7f","2c:33:11:43:09:7d"},
+                    new String[] {"00:a3:8e:8f:be:7e","00:a3:8e:8f:be:7f","00:a3:8e:8f:be:7d","00:a3:8e:8f:b4:4e","00:a3:8e:8f:b4:4f","00:a3:8e:8f:b4:4d","2c:33:11:43:05:ef","2c:33:11:43:05:ee","2c:33:11:43:05:ed","70:db:98:26:7c:5f","70:db:98:26:7c:5e","70:db:98:26:7c:5d","00:a3:8e:8f:b4:42","00:a3:8e:8f:b4:41","2c:33:11:43:09:7e","2c:33:11:43:09:7f","2c:33:11:43:09:7d","2c:d0:2d:f5:85:3e","2c:d0:2d:f5:85:3d","00:a3:8e:8f:b4:40","2c:d0:2d:f5:85:3f","2c:d0:2d:f5:85:32","2c:d0:2d:f5:85:31","2c:33:11:43:05:e1","2c:33:11:43:05:e2"}
 
             };
     private final Float[][] weights = new Float[][]//static to determine where I am
@@ -91,18 +96,18 @@ public class MainActivity extends Activity implements OnClickListener, SensorEve
                     new Float[]{(float)(1),(float)(1),(float)(0.85),(float)(0.85),(float)(0.85),(float)(0.7),(float)(0.65),(float)(0.35),(float)(0.3),(float)(0.25),(float)(0.15),(float)(0.15),(float)(0.15),(float)(0.1),(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0},//147
                     new Float[]{(float)(1),(float)(1),(float)(0.85),(float)(0.85),(float)(0.75),(float)(0.4),(float)(0.4),(float)(0.4),(float)(0.35),(float)(0.15),(float)(0.1),(float)(0.1),(float)(0.05),(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0}//128
 */
-                    new Float[]{(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1},
-                    new Float[]{(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1},
-                    new Float[]{(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1},
-                    new Float[]{(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1}
+                    new Float[]{(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1},
+                    new Float[]{(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1},
+                    new Float[]{(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1},
+                    new Float[]{(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1,(float) 1}
             };
     private Integer[][] booleans = new Integer[][]//static to determine where I am
             {
 
-                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 
   /*                  new Integer[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
                     new Integer[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -232,7 +237,7 @@ public class MainActivity extends Activity implements OnClickListener, SensorEve
 
 
         int counter =0;
-        int numwifis=35; // variable of max number of wifi founded in all cells
+        int numwifis=45; // variable of max number of wifi founded in all cells
 
         //What I need to do is to take as reference my array of scanResults and create a new array with results of booleans
 
@@ -241,7 +246,7 @@ public class MainActivity extends Activity implements OnClickListener, SensorEve
         //List<ScanResult> scanResults = wifiManager.getScanResults();
 
         for (int i=0; i < numbelem; i++){
-            //1 to 4
+            //1 to 4 
             counter =0;
 
             for (String nameSSID:trainingArray[i]){
