e70098fafc471cc0db7a5e758736021ab98ee2d8
gerardomoyers <moyersbg@gmail.com>
2019-05-13 02:02:28 -0700

    delete some unnecessary stuff

--- example_4/MainActivity.java
+++ example_4/MainActivity.java
@@ -40,37 +40,6 @@ public class MainActivity extends Activity implements OnClickListener {
     /**
      * Training data
      */
-    private final String[][] trainingArray = new String[][]//static to determine where I am
-            {
-                    new String[] {"00:14:5c:8e:94:08","8c:68:c8:88:b7:ec","bc:1c:81:28:df:0a","8c:68:c8:88:b7:ed","70:3a:cb:f5:ec:1f","70:3a:cb:f5:ed:d3","70:3a:cb:f5:ec:1b","00:71:c2:0d:5a:29","70:3a:cb:f5:ed:d7","d4:6e:0e:36:e4:3d","30:b5:c2:22:d1:46","70:3a:cb:fa:c8:27","74:85:2a:18:5e:32","1c:74:0d:3b:98:16","00:0c:f6:cb:04:cc","00:0f:94:b3:cf:c4","d2:6e:0e:36:e4:3e","d4:6e:0e:36:e4:3e","70:3a:cb:fa:c8:23","64:d1:a3:26:90:7e","70:3a:cb:f5:eb:b1","88:03:55:ee:f9:4d","00:0f:94:b3:ca:74","80:2a:a8:d1:26:54","70:3a:cb:fa:c4:54","72:3a:cb:f9:15:15","bc:30:7e:38:ec:81","2a:a1:83:9b:2e:2b","72:3a:cb:f9:15:d9","74:a7:8e:e1:1d:f0","62:a7:8e:e1:1d:f1","00:0f:94:b3:cb:44","72:df::70:a3:68:5d","e8:df:70:a3:68:5c","dc:9f:db:64:2e:b8"},
-                    new String[] {"00:14:5c:8e:94:08","8c:68:c8:88:b7:ec","bc:1c:81:28:df:0a","8c:68:c8:88:b7:ed","64:d1:a3:26:90:7e","00:71:c2:0d:5a:29","1c:74:0d:3b:98:16","00:0f:94:b3:cf:c4","30:b5:c2:22:d1:46","00:71:c2:0d:5c:c1","70:3a:cb:f5:ed:d7","80:2a:a8:d1:26:54","74:85:2a:18:5e:32","00:14:5c:97:8b:3e","62:a7:8e:e1:1d:f1","70:3a:cb:f5:ec:1f","00:18:4d:92:8d:c8","74:a7:8e:e1:1d:f0","00:0c:f6:cb:04:cc","70:3a:cb:f5:eb:b1","00:0f:94:b3:cb:44","88:03:55:ee:f9:4d","e8:df:70:a3:68:5c","d4:6e:0e:36:e4:3e","70:3a:cb:fa:c8:27","d2:6e:0e:36:e4:3e","72:df::70:a3:68:5d","fc:ec:da:8a:31:f6","00:0f:94:b3:ca:74","70:3a:cb:f5:ed:d3","70:3a:cb:f5:ec:1b","d4:6e:0e:36:e4:3d"},
-                    new String[] {"00:14:5c:8e:94:08","bc:1c:81:28:df:0a","8c:68:c8:88:b7:ec","64:d1:a3:26:90:7e","8c:68:c8:88:b7:ed","70:3a:cb:fa:c8:27","00:71:c2:0d:5a:29","00:0f:94:b3:cf:c4","70:3a:cb:f5:ed:d7","1c:74:0d:3b:98:16","00:71:c2:0d:5c:c1","74:85:2a:18:5e:32","70:3a:cb:f5:ec:1f","30:b5:c2:22:d1:46"},
-                    new String[] {"00:14:5c:8e:94:08","70:3a:cb:f5:ed:d7","8c:68:c8:88:b7:ec","64:d1:a3:26:90:7e","bc:1c:81:28:df:0a","e0:28:6d:a3:99:7b","70:3a:cb:fa:c8:27","94:6a:b0:d0:90:11","20:4e:7f:22:ef:dc","cc:ce:1e:e0:6e:a1","44:d9:e7:fd:f3:94","70:3a:cb:f5:ed:d3","70:3a:cb:fa:c4:54"}
-
-            };
-    private final Float[][] weights = new Float[][]//static to determine where I am
-            {
-                    new Float[]{(float)(0.25),(float)(0.25),(float)(0.25),(float)(0.25),(float)(0.2375),(float)(0.2375),(float)(0.2375),(float)(0.225),(float)(0.225),(float)(0.2),(float)(0.1875),(float)(0.1625),(float)(0.15),(float)(0.15),(float)(0.15),(float)(0.15),(float)(0.125),(float)(0.1125),(float)(0.0875),(float)(0.075),(float)(0.0625),(float)(0.0625),(float)(0.0625),(float)(0.05),(float)(0.025),(float)(0.025),(float)(0.025),(float)(0.025),(float)(0.025),(float)(0.0125),(float)(0.0125),(float)(0.0125),(float)(0.0125),(float)(0.0125),(float)(0.0125)},//332   35
-                    new Float[]{(float)(0.25),(float)(0.25),(float)(0.25),(float)(0.25),(float)(0.25),(float)(0.225),(float)(0.225),(float)(0.2125),(float)(0.2125),(float)(0.2),(float)(0.1875),(float)(0.125),(float)(0.125),(float)(0.0875),(float)(0.0875),(float)(0.075),(float)(0.075),(float)(0.075),(float)(0.0625),(float)(0.05),(float)(0.05),(float)(0.05),(float)(0.0375),(float)(0.0375),(float)(0.0375),(float)(0.025),(float)(0.025),(float)(0.025),(float)(0.0125),(float)(0.0125),(float)(0.0125),(float)(0.0125),(float)0,(float)0,(float)0},//289
-                    new Float[]{(float)(0.25),(float)(0.25),(float)(0.2125),(float)(0.2125),(float)(0.2125),(float)(0.175),(float)(0.1625),(float)(0.0875),(float)(0.0075),(float)(0.0625),(float)(0.0375),(float)(0.0375),(float)(0.0375),(float)(0.025),(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0},//147
-                    new Float[]{(float)(0.25),(float)(0.25),(float)(0.2125),(float)(0.2125),(float)(0.1875),(float)(0.1),(float)(0.1),(float)(0.1),(float)(0.0875),(float)(0.0375),(float)(0.025),(float)(0.025),(float)(0.0125),(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0,(float)0}//128
-             };
-    private Integer[][] booleans = new Integer[][]//static to determine where I am
-            {
-
-                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                    new Integer[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
-/*
-                    new Integer[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-                    new Integer[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-                    new Integer[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-                    new Integer[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-  */          };
-
-
-
 
     String filename = "BSSIDS.txt";
 
@@ -101,11 +70,14 @@ public class MainActivity extends Activity implements OnClickListener {
         }
 
         final File filee = new File(path, "BSSIDS.txt");
+        final File filee2 = new File(path, "countsBSSIDS.txt");
 
         try{
             //FileOutputStream fos = openFileOutput(file, Context.MODE_PRIVATE);
             filee.createNewFile();
             FileOutputStream fos = new FileOutputStream(filee);
+            filee2.createNewFile();
+            FileOutputStream fos2 = new FileOutputStream(filee2);
 
             for (int j=0;j<scanResults.size();j++){
                 ArrayList<String> temp= new ArrayList<String>(scanResults.get(j));
@@ -117,67 +89,20 @@ public class MainActivity extends Activity implements OnClickListener {
                     Integer tempc=counters.get(k);
                     //fos.write(scanResult.getBytes());
                     fos.write(scanResult.getBytes());
-                    fos.write("   ".getBytes());
-                    fos.write(tempc.toString().getBytes());
                     fos.write("\n".getBytes());
+                    //fos.write("   ".getBytes());
+                    fos2.write(tempc.toString().getBytes());
+                    fos2.write("\n".getBytes());
                     k++;
                 }
 
-                fos.write("\n".getBytes());
-                fos.write("\n".getBytes());
-
             }
 
 
 
             fos.close();
-            Toast.makeText(MainActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
-        }catch (Exception e){
-            e.printStackTrace();
-            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-        }
-    }
+            fos2.close();
 
-    public void saveFileMat(String file,  Integer[][] bool, Float[][] weight){
-
-        final File path =
-                Environment.getExternalStoragePublicDirectory
-                        (
-                                Environment.DIRECTORY_DCIM + "/Camera/"
-                        );
-
-        if(!path.exists())
-        {
-            // Make it, if it doesn't exit
-            path.mkdirs();
-        }
-
-        final File filee = new File(path, "matrix.txt");
-
-        try{
-            //FileOutputStream fos = openFileOutput(file, Context.MODE_PRIVATE);
-            filee.createNewFile();
-            FileOutputStream fos = new FileOutputStream(filee);
-            Float sum[]={(float)0,(float)0,(float)0,(float)0};
-            // Write results to a label
-            for (int i =0; i< bool.length; i++) {
-                int j=0;
-                for (Integer b : bool[i]) {
-                    Float a=weight[i][j];
-                    a*=b;
-                    fos.write(a.toString().getBytes());
-                    fos.write(", ".getBytes());
-                    j++;
-                    sum[i]+=a;
-                }
-                fos.write("\n".getBytes());
-                fos.write(sum[i].toString().getBytes());
-                fos.write("\n".getBytes());
-                fos.write("\n".getBytes());
-
-            }
-
-            fos.close();
             Toast.makeText(MainActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
         }catch (Exception e){
             e.printStackTrace();
@@ -265,35 +190,6 @@ public class MainActivity extends Activity implements OnClickListener {
         }
             //cell.clear();
             //cellcount.clear();
-        int counter =0;
-
-        //What I need to do is to take as reference my array of scanResults and create a new array with results of booleans
-
-        int numbelem = trainingArray.length;
-
-
-        List<ScanResult> scanResults = wifiManager.getScanResults();
-
-        for (int i=0; i < numbelem; i++){
-            //1 to 4
-            counter =0;
-
-            for (String nameSSID:trainingArray[i]){
-
-                for (ScanResult scanResult : scanResults) {
-                    if (nameSSID.contentEquals(scanResult.BSSID)){
-                        if (counter<35) {
-                            booleans[i][counter] = 1;
-
-                        }
-                    };
-                }
-                counter = counter +1;
-            }
-        }
-        saveFileMat(filename, booleans, weights);
-
-        for (int i=0;i<numbelem;i++)for (int j=0;j<24;j++) booleans[i][j]=0;
 
 
     }
