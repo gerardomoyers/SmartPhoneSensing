a4baa3d10161d4a50125acc67db7bb6eefea8609
Unknown <teresablancoabad95@gmail.com>
2019-06-10 11:53:48 +0200

    After refactoring - motion model

--- particleFilters/motionModel/MainActivity.java
+++ particleFilters/motionModel/MainActivity.java
@@ -150,27 +150,7 @@ public class MainActivity extends Activity implements OnClickListener {
                     }
 
 
-//                    if ((now - lastDetected) > timeout*1000000){
-//                        totalSteps = (int) event.values[0] - reportedSteps - lastSteps;
-//                    }
-//                    else{
-//                        totalSteps= (int) event.values[0] - reportedSteps;
-//                    }
-//
-//                    lastDetected = event.timestamp;
-
-                    // Calculate steps taken based on
-                    // first value received.
-
                     stepsTaken = (int) event.values[0] - reportedSteps;
-                    //lastSteps = stepsTaken;
-                    //showToast("result" + stepsTaken);
-
-                    // Output the value to the simple GUI
-                    //showToast("Cnt: " + stepsTaken);
-                    //countText.setText("Cnt: " + stepsTaken);
-                    //showToast("Det: " + stepsTaken);
-                    //Log.e("here", "totalSteps"+totalSteps);
 
                     break;
 
@@ -220,15 +200,6 @@ public class MainActivity extends Activity implements OnClickListener {
 
 
                 case Sensor.TYPE_ROTATION_VECTOR:
-//                    if (event.values.length > 4) {
-//                        float[] truncatedRotationVector = new float[4];
-//                        System.arraycopy(event.values, 0, truncatedRotationVector, 0, 4);
-//                        update(truncatedRotationVector);
-//                    } else {
-//                        update(event.values);
-//                    }
-
-
                     Rotation rotation = new Rotation(
                             (double) event.values[3], // quaternion scalar
                             (double) event.values[0], // quaternion x
@@ -300,23 +271,6 @@ public class MainActivity extends Activity implements OnClickListener {
 
 
 
-public void update(float[] vectors) {
-
-        final int FROM_RADS_TO_DEGS = -57;
-        float[] rotationMatrix = new float[9];
-        SensorManager.getRotationMatrixFromVector(rotationMatrix, vectors);
-        int worldAxisX = SensorManager.AXIS_X;
-        int worldAxisZ = SensorManager.AXIS_Z;
-        float[] adjustedRotationMatrix = new float[9];
-        SensorManager.remapCoordinateSystem(rotationMatrix, worldAxisX, worldAxisZ, adjustedRotationMatrix);
-        float[] orientation = new float[3];
-        SensorManager.getOrientation(adjustedRotationMatrix, orientation);
-        float yaw = orientation[2] * FROM_RADS_TO_DEGS;
-
-        Log.e("here","yawInside"+yaw);
-        };
-
-
     public void StdGaussian() {
 
             double r, x, y;
@@ -337,136 +291,6 @@ public void update(float[] vectors) {
 
     };
 
-
-
-
-
-/*
-    //start
-
-    public interface StepListener {
-        public void onStep();
-        public void passValue();
-    }
-
-
-
-
-
-    final SensorEventListener thiss = new SensorEventListener() {
-
-//        private final static String TAG = "StepDetector";
-//        private float   mLimit = 10;
-//        private float   mLastValues[] = new float[3*2];
-//        private float   mScale[] = new float[2];
-//        private float   mYOffset;
-//
-//        private float   mLastDirections[] = new float[3*2];
-//        private float   mLastExtremes[][] = { new float[3*2], new float[3*2] };
-//        private float   mLastDiff[] = new float[3*2];
-//        private int     mLastMatch = -1;
-
-        private ArrayList<StepListener> mStepListeners = new ArrayList<StepListener>();
-
-
-
-//        public void thiss() {
-//            int h = 480; // TODO: remove this constant
-//            mYOffset = h * 0.5f;
-//            mScale[0] = - (h * 0.5f * (1.0f / (SensorManager.STANDARD_GRAVITY * 2)));
-//            mScale[1] = - (h * 0.5f * (1.0f / (SensorManager.MAGNETIC_FIELD_EARTH_MAX)));
-//        }
-
-
-//        public void setSensitivity(float sensitivity) {
-//            mLimit = sensitivity; // 1.97  2.96  4.44  6.66  10.00  15.00  22.50  33.75  50.62
-//        }
-
-        public void addStepListener(StepListener sl) {
-            mStepListeners.add(sl);
-        }
-
-        @Override
-        //public void onSensorChanged(int sensor, float[] values) {
-        public void onSensorChanged(SensorEvent event) {
-            Sensor sensor = event.sensor;
-
-            //final static String TAG = "StepDetector";
-            float   mLimit = (float) 1.97;
-            float   mLastValues[] = new float[3*2];
-            float   mScale[] = new float[2];
-            float   mYOffset;
-
-            float   mLastDirections[] = new float[3*2];
-            float   mLastExtremes[][] = { new float[3*2], new float[3*2] };
-            float   mLastDiff[] = new float[3*2];
-            int     mLastMatch = -1;
-
-
-
-
-
-            int h = 480; // TODO: remove this constant
-            mYOffset = h * 0.5f;
-            mScale[0] = - (h * 0.5f * (1.0f / (SensorManager.STANDARD_GRAVITY * 2)));
-            mScale[1] = - (h * 0.5f * (1.0f / (SensorManager.MAGNETIC_FIELD_EARTH_MAX)));
-
-            synchronized (this) {
-                if (sensor.getType() == Sensor.TYPE_ORIENTATION) {
-                }
-                else {
-                    int j = (sensor.getType() == Sensor.TYPE_ACCELEROMETER) ? 1 : 0;
-                    if (j == 1) {
-                        float vSum = 0;
-                        for (int i=0 ; i<3 ; i++) {
-                            final float v = mYOffset + event.values[i] * mScale[j];
-                            vSum += v;
-                        }
-                        int k = 0;
-                        float v = vSum / 3;
-
-                        float direction = (v > mLastValues[k] ? 1 : (v < mLastValues[k] ? -1 : 0));
-                        if (direction == - mLastDirections[k]) {
-                            // Direction changed
-                            int extType = (direction > 0 ? 0 : 1); // minumum or maximum?
-                            mLastExtremes[extType][k] = mLastValues[k];
-                            float diff = Math.abs(mLastExtremes[extType][k] - mLastExtremes[1 - extType][k]);
-
-                            if (diff > mLimit) {
-
-                                boolean isAlmostAsLargeAsPrevious = diff > (mLastDiff[k]*2/3);
-                                boolean isPreviousLargeEnough = mLastDiff[k] > (diff/3);
-                                boolean isNotContra = (mLastMatch != 1 - extType);
-
-                                if (isAlmostAsLargeAsPrevious && isPreviousLargeEnough && isNotContra) {
-                                    Log.i("here", "step");
-                                    for (StepListener stepListener : mStepListeners) {
-                                        stepListener.onStep();
-                                    }
-                                    mLastMatch = extType;
-                                }
-                                else {
-                                    mLastMatch = -1;
-                                }
-                            }
-                            mLastDiff[k] = diff;
-                        }
-                        mLastDirections[k] = direction;
-                        mLastValues[k] = v;
-                    }
-                }
-            }
-        }
-
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            // TODO Auto-generated method stub
-        }
-
-    };
-
-    //end
-
-*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -559,67 +383,8 @@ public void update(float[] vectors) {
 
     @Override
     public void onClick(View v) {
-        // This happens when you click any of the four buttons.
-        // For each of the buttons, when it is clicked we change:
-        // - The text in the center of the buttons
-        // - The margins
-        // - The text that shows the margin
-        float startLat = 0;
-        float startLon = 0;
-        int altitude = 0;
-        int time = 0;
-        float maybe = 0;
-        double floatBearing = 0;
 
-        if (SensorManager.getRotationMatrix(rMat, iMat, gData, mData)) {
-
-            Float azz = SensorManager.getOrientation(rMat, orientation)[0];
-
-            mAzimuth = (float) ((Math.toDegrees(azz)+ 360) % 360.0);
-
-
-
-            Log.e("this", ""+mAzimuth);
-
-            /*
-            GeomagneticField geo = new GeomagneticField((float) startLat, (float) startLon, altitude, time);
-            float decl = geo.getDeclination();
-
-            if (orientation[0] >= 0) {
-                // Azimuth-Calculation (rad in degree) + difference to true north (decl)
-                maybe = (orientation[0] * 57.29577951f + decl);
-            } else {
-                // Azimuth-Calculation (rad in degree) +360 + difference to true north (decl)
-                maybe = (orientation[0] * 57.29577951f + 360 + decl);
-            }
-
-
-            floatBearing = orientation[0];
-            floatBearing = Math.toDegrees(floatBearing);
-            if (geo != null)
-                floatBearing += geo.getDeclination();
-            if (floatBearing < 0)
-                floatBearing += 360;
-        context = this;
-
-
-
-
-
-        }
-
-        //showToast("hihi");
-        Log.e("hihi", "hey");
-        Log.e("hihi", "hey"+mAzimuth);
-        Log.e("second trial", "h"+maybe);
-        Log.e("third", "j"+floatBearing);
-
-        //showToast("Here"+mAzimuth);
-*/
             //calculate distance
-            floatBearing = 0;
-
-
             Double[] distance = new Double[]{(double) 0, (double) 0, (double) 0, (double) 0, (double) 0};
 
             //where was I? use of x and y
@@ -641,57 +406,8 @@ public void update(float[] vectors) {
 
             }
 
-//        showToast("angle: " +floatBearing);
-//        showToast("two"+ mAzimuth);
-//        showToast("tre"+maybe);
-
-
-//        switch (v.getId()) {
-//            // UP BUTTON
-//            case R.id.button1: {
-//                Toast.makeText(getApplication(), "UP", Toast.LENGTH_SHORT).show();
-//                Rect r = drawable.getBounds();
-//                drawable.setBounds(r.left,r.top-20,r.right,r.bottom-20);
-//                textView.setText("\n\tMove Up" + "\n\tTop Margin = "
-//                        + drawable.getBounds().top);
-//                break;
-//            }
-//            // DOWN BUTTON
-//            case R.id.button4: {
-//                Toast.makeText(getApplication(), "DOWN", Toast.LENGTH_SHORT).show();
-//                Rect r = drawable.getBounds();
-//                drawable.setBounds(r.left,r.top+20,r.right,r.bottom+20);
-//                textView.setText("\n\tMove Down" + "\n\tTop Margin = "
-//                        + drawable.getBounds().top);
-//                break;
-//            }
-//            // LEFT BUTTON
-//            case R.id.button2: {
-//                Toast.makeText(getApplication(), "LEFT", Toast.LENGTH_SHORT).show();
-//                Rect r = drawable.getBounds();
-//                drawable.setBounds(r.left-20,r.top,r.right-20,r.bottom);
-//                textView.setText("\n\tMove Left" + "\n\tLeft Margin = "
-//                        + drawable.getBounds().left);
-//                break;
-//            }
-//            // RIGHT BUTTON
-//            case R.id.button3: {
-//                Toast.makeText(getApplication(), "RIGHT", Toast.LENGTH_SHORT).show();
-//                Rect r = drawable.getBounds();
-//                drawable.setBounds(r.left+20,r.top,r.right+20,r.bottom);
-//                textView.setText("\n\tMove Right" + "\n\tLeft Margin = "
-//                        + drawable.getBounds().left);
-//                break;
-//            }
-//        }
-
-//        // redrawing of the object
-//        canvas.drawColor(Color.WHITE);
-//        drawable.draw(canvas);
-//        for(ShapeDrawable wall : walls)
-//            wall.draw(canvas);
-        }
     }
+
 
     @Override
     public void onResume()
